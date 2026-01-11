733b4010beaf1c1e6dc5145d5971077eb252b763
Jaydeep Karpe <karpepatil6@gmail.com>
2026-01-11 17:48:36 +0530

    Mega project of complex blog

---
+++ 12megaBlog/src/pages/EditPost.jsx
@@ -0,0 +1,33 @@
+import React, {useState, useEffect, use} from 'react'
+import { Container, PostFrom} from '../components'
+import appwriteService from '../appwrite/config'
+import { useNavigate, useParams } from 'react-router-dom'
+
+function EditPost() {
+    const [post, setPost] = useState(null)
+    const {slug} = useParams()
+    const navigate = useNavigate()  
+
+    useEffect(() => {
+        if (slug) {
+            appwriteService.getPost(slug).then((post) =>{
+                if (post) {
+                    setPost(post)
+                } 
+            })
+        } else {
+            navigate('/')
+        }            
+                       
+    }, [ slug, navigate])
+    
+  return post ? (
+    <div className='py-8'>
+        <Container>
+            <PostFrom post={post} />
+        </Container>    
+    </div>
+  ) : null 
+}
+
+export default EditPost
